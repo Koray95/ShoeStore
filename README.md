90172e3c77359b3d0a54fe8589d9fa8a021bfc73
koray95 <koraydogan95@gmail.com>
2022-03-01 11:12:53 +0300

    entities

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # ShoeStore
 
+### Project Structure
+```
+/src
+* ApplicationCore
+* Infrastructure
+*Web
 
+/tests
+* UnitTests
+
+```
+
+### Packages
+```
+/ApplicationCore
+Install-Package Ardalis.Specification -v 5.2.0
+```
 
 ### Resources
 
---
+++ src/ApplicationCore/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUri { get; set; } // Uri: Unique resource Identity  Uri>Url
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
